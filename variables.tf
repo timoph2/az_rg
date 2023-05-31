87c23e5f9a35661447d0dfe88770ae7e77f905e9
timoph2 <98256868+timoph2@users.noreply.github.com>
2023-05-31 15:40:53 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "location" {
  
   validation {
 #     condition     = var.location == var.location ? 1 : 2
-    condition     = " " != "" ? "" : "default-a"
+    condition     = var.location != "" ? "" : "default-a"
    
 
 
