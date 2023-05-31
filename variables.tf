46d6df45a44b74c4f0e7ba1c4e7e7e685eeaa0ee
timoph2 <98256868+timoph2@users.noreply.github.com>
2023-05-31 13:40:08 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,7 +13,12 @@ variable "location" {
 #     condition     = var.location == ""
 #     condition     = var.location == var.location 
 
-    condition     = contains( [var.location],var.location) 
+   locals {
+  test = var.location == var.location
+}
+   
+    condition     = locals.test 
+#     condition     = contains( [var.location],var.location) 
     error_message = "Must be either `least-waste`, `most-pods`, `priority` or `random`."
   }
 }
