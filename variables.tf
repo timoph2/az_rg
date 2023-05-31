2739e552a0ab6e2c5c90b2dc9cad73a2b645c801
timoph2 <98256868+timoph2@users.noreply.github.com>
2023-05-31 13:16:52 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "location" {
   default     = "UK South"
  
   validation {
-    condition     = var.location
+    condition     = var.location == var.location
 #     condition     = length(var.location) > 0
 #     error_message = "Must be either `least-waste`, `most-pods`, `priority` or `random`."
     error_message = "Must be more than 0. "
