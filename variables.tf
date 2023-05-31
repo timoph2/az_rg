3dfd615b08ea79a399269d348c705ca415f368ab
timoph2 <98256868+timoph2@users.noreply.github.com>
2023-05-31 14:19:58 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,12 +14,13 @@ variable "location" {
 #     condition     = var.location == var.location 
 
    locals {
-  test = contains( [var.location],var.location )   #collection functions
+#   test = contains( [var.location],var.location )   #collection functions
     test = true 
     
 #   test2 = floor(4.9) #numeric function 
 #   test2 = endswith("hello world", "world") #string function works
 #   test2 = abspath(path.root) #file function works
+  test2 = cidrhost("10.12.112.0/20", 16) #IP Network function works
     
 }
    
