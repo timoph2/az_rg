efebc422c5beab9c417c102e0bc9f5720476fbb6
timoph2 <98256868+timoph2@users.noreply.github.com>
2023-05-31 14:18:09 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,14 +14,12 @@ variable "location" {
 #     condition     = var.location == var.location 
 
    locals {
-#   test = contains( [var.location],var.location ) 
+  test = contains( [var.location],var.location )   #collection functions
     test = true 
-#   test2 =  sort(["e", "d", "a", "x"]) #collection functions
     
 #   test2 = floor(4.9) #numeric function 
 #   test2 = endswith("hello world", "world") #string function works
-     
-  test2 = abspath(path.root) #file function 
+#   test2 = abspath(path.root) #file function works
     
 }
    
