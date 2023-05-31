a18191df8f985e5e7aad5f9aaf8f59d8440b7e90
timoph2 <98256868+timoph2@users.noreply.github.com>
2023-05-31 14:33:20 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,24 +10,19 @@ variable "location" {
   default     = "UK South"
  
   validation {
-#     condition     = var.location == ""
-#     condition     = var.location == var.location 
-
-   locals {
-#   test = contains( [var.location],var.location )   #collection functions
-    test = true 
-    
-#   test2 = floor(4.9) #numeric function 
-#   test2 = endswith("hello world", "world") #string function works
-#   test2 = abspath(path.root) #file function works
-#   test2 = cidrhost("10.12.112.0/20", 16) #IP Network function 
-  test2 = tobool(true) #Type conversion function 
-    
-    
-}
-   
-    condition     = local.test 
-#     condition     = contains( [var.location],var.location) 
+    condition     = var.location == var.location 
     error_message = "Must be either `least-waste`, `most-pods`, `priority` or `random`."
   }
 }
+
+#    locals {
+# #   test = contains( [var.location],var.location )   #collection functions
+#     test = true 
+    
+# #   test2 = floor(4.9) #numeric function 
+# #   test2 = endswith("hello world", "world") #string function works
+# #   test2 = abspath(path.root) #file function works
+# #   test2 = cidrhost("10.12.112.0/20", 16) #IP Network function 
+#     test2 = tobool(true) #Type conversion function  
+
+
